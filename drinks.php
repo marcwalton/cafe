89628cc93a537e0e387a35a0999ce5596f5c3b04
marcwalton <63082762+marcwalton@users.noreply.github.com>
2020-06-29 15:08:43 +1200

    Testing New Message

--- drinks.php
+++ drinks.php
@@ -4,7 +4,7 @@
     if(mysqli_connect_errno()){
         echo "Failed to connect to MySQL:".mysqli_connect_error(); die();}
     else {
-        echo "connected to database";
+        echo "connected to databaseee";
     }
 
     if(isset($_GET['drink'])){
